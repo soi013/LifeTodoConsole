22e5a5907c62d20ba37c05d76b23fa77595bb87e
soi013 <soi2flatline@gmail.com>
2024-09-25 21:59:05 +0900

    テストコードの追加

---
+++ Test/Todo_Test.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using LifeTodoConsole.Domain;
+
+namespace Test
+{
+    public class Todo_Test
+    {
+        [Fact]
+        public void Todo_CreateTodo_Success()
+        {
+            var todo = new Todo("hoge", new DateTime(2020, 12, 3));
+            todo.Should().NotBeNull();
+            todo.Text.Should().Be("hoge");
+            todo.CreatedDate.Year.Should().Be(2020);
+            todo.CreatedDate.Month.Should().Be(12);
+            todo.CreatedDate.Day.Should().Be(3);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Todo_CreateInvalidTextTodo_Fail(string textTodo)
+        {
+
+            var funcTodo = () => new Todo(textTodo, new DateTime(2020, 12, 3));
+            funcTodo.Should().Throw<ArgumentException>();
+        }
+    }
+}
